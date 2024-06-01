e8018bb712dc6771cdd7267efe675fe3ec6c4a81
DESKTOP-PR8MKLN\hrodland <7200ws@gmail.com>
2024-06-01 13:05:46 -0500

    IMC

--- app/src/main/java/com/hrod/androidmaster/imccalculator/ImcCanculatorActivity.kt
+++ app/src/main/java/com/hrod/androidmaster/imccalculator/ImcCanculatorActivity.kt
@@ -1,24 +1,118 @@
 package com.hrod.androidmaster.imccalculator
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
 import com.hrod.androidmaster.R
 
 class ImcCanculatorActivity : AppCompatActivity() {
+
+    private var isMaleSelected:Boolean = true
+    private var isFemaleSelected:Boolean = false
+    private var currentWeight: Int = 70
+
+
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
+
+    private  lateinit var tvHeight:TextView
+    private  lateinit var rsHeight:RangeSlider
+
+    private lateinit var btnSubtractWeight: FloatingActionButton
+    private lateinit var btnPlusWeight: FloatingActionButton
+    private  lateinit var tvWeight: TextView
+
+    private lateinit var btnSubtractAge: FloatingActionButton
+    private lateinit var btnPlusAge: FloatingActionButton
+    private  lateinit var tvAge: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_imc_canculator)
-
-
-
+        initComponents()
+        initListeners()
+        initUI()
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
+    }
+
+
+    private fun initComponents(){
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight)
+        btnSubtractWeight = findViewById(R.id.btnSubtractWeight)
+        btnPlusWeight = findViewById(R.id.btnPlusWeight)
+        tvWeight = findViewById(R.id.tvWeight)
+    }
+
+    private fun initListeners(){
+        viewMale.setOnClickListener{
+            changeGender()
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener{
+            changeGender()
+            setGenderColor()
+        }
+
+        rsHeight.addOnChangeListener{_,value,_->
+            val df = DecimalFormat("#.##")
+            val result = df.format(value)
+            tvHeight.text = result
+        }
+        btnPlusWeight.setOnClickListener{
+            currentWeight += 1
+            setWeight()
+        }
+        btnSubtractWeight.setOnClickListener{
+            currentWeight -= 1
+            setWeight()
+        }
+
+    }
+
+    private  fun initUI(){
+        setGenderColor()
+        setWeight()
+    }
+
+    private fun setWeight(){
+        tvWeight.text = currentWeight.toString()
+    }
+
+    private fun changeGender(){
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
+    }
+
+    private fun setGenderColor(){
+        viewMale.setBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setBackgroundColor(getBackgroundColor(isMaleSelected))
+    }
+
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int{
+
+        val colorReference = if(isSelectedComponent){
+            R.color.background_component_selected
+        }else{
+            R.color.background_component
+        }
+
+        return ContextCompat.getColor(this, colorReference)
+
     }
 }
